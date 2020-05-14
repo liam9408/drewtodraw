12ad5d6ee37f435665d2bb184eebb3fe8b72bf9d
liam9408 <57433615+liam9408@users.noreply.github.com>
2020-05-14 20:01:48 +0800

    added viewRouter and done loads on frontend

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-//-------------- Setting up required packages 
-const express = require('express');
+//-------------- Setting up required packages
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const https = require('https')
-const fs = require('fs')
-const path = require('path')
-const hbs = require('express-handlebars');
-const pg = require('pg');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const hbs = require("express-handlebars");
+const pg = require("pg");
+const gallery = require("./public/photos");
 
 //-------------- General package setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/public')));
-require('dotenv').config()
+app.use(express.static(path.join(__dirname, "/public")));
+require("dotenv").config();
+
+//-------------- Router package setup
+const ViewRouter = require("./Router/viewRouter")(express);
 
 //-------------- setting up database connection
-const knexConfig = require('./knexfile').development;
-const knex = require('knex')(knexConfig);
-
-
+const knexConfig = require("./knexfile").development;
+const knex = require("knex")(knexConfig);
 
 //-------------- Handlebars setup
-// const viewRouter = require('./router/viewRouter')(express);
-// app.engine('handlebars', hbs({ defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/', partialsDir: __dirname + '/views/partials/' }));
-// app.set('view engine', 'handlebars');
+// const viewRouter = require("./router/viewRouter")(express);
+app.engine(
+  "handlebars",
+  hbs({
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
+app.set("view engine", "handlebars");
 
 //-------------- services setup
-
-
 
 //-------------- authentication setup
 // const setupPassport = require('./authentication/initPassport');
@@ -42,22 +49,19 @@ const knex = require('knex')(knexConfig);
 // );
 // setupPassport(app);
 
-
-
 //-------------- Routers routing
-// app.use('/', viewRouter);
-// app.use('/', loginRouter);
-// app.use('/api/projects', new projectRouter(projectService).router());
-// app.use('/api/tasks', new taskRouter(taskService).router());
-// app.use('/api/subtasks', new subtaskRouter(subtaskService).router());
+app.use("/", ViewRouter);
+// app.use("/", loginRouter);
+// app.use("/api/projects", new projectRouter(projectService).router());
+// app.use("/api/tasks", new taskRouter(taskService).router());
+// app.use("/api/subtasks", new subtaskRouter(subtaskService).router());
 
-const options ={
-  cert: fs.readFileSync('./localhost.crt'),
-  key: fs.readFileSync('./localhost.key')
-}
+const options = {
+  cert: fs.readFileSync("./localhost.crt"),
+  key: fs.readFileSync("./localhost.key"),
+};
 
 const PORT = process.env.PORT;
 https.createServer(options, app).listen(PORT, () => {
-    console.log(`Server running at ${PORT}.`);
-  });
-
+  console.log(`Server running at ${PORT}.`);
+});
---
+++ router/viewRouter.js
@@ -0,0 +1,29 @@
+const gallery = require("../public/photos");
+
+module.exports = (express) => {
+  const router = express.Router();
+
+  // define middleware
+  //   function isLoggedIn(req, res, next) {
+  // check if user is currently logged in
+  // if (req.isAuthenticated()) {
+  //   return next();
+  // }
+  // if not logged in, redirect to the landing page
+  // res.render("login");
+  // }
+
+  router.get("/", (req, res) => {
+    res.render("work");
+  });
+
+  router.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+  router.get("/contact", (req, res) => {
+    res.render("contact");
+  });
+
+  return router;
+};
