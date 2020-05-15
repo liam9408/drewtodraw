94f8cbd63d561d5ca035f25032bc18c8da94dd69
liam9408 <57433615+liam9408@users.noreply.github.com>
2020-05-15 15:29:34 +0800

    added edit page and login

--- public/js/custom-work.js
+++ public/js/custom-work.js
@@ -1,256 +1,205 @@
-(function($) { "use strict";
+(function ($) {
+  "use strict";
 
-	function Selector_Cache() {
-		var collection = {};
+  function Selector_Cache() {
+    var collection = {};
 
-		function get_from_cache( selector ) {
-			if ( undefined === collection[ selector ] ) {
-				collection[ selector ] = $( selector );
-			}
+    function get_from_cache(selector) {
+      if (undefined === collection[selector]) {
+        collection[selector] = $(selector);
+      }
 
-			return collection[ selector ];
-		}
+      return collection[selector];
+    }
 
-		return { get: get_from_cache };
-	}
+    return { get: get_from_cache };
+  }
 
-	var selectors = new Selector_Cache();
+  var selectors = new Selector_Cache();
 
+  //Preloader
 
+  Royal_Preloader.config({
+    mode: "progress",
+    background: "#ffffff",
+    showProgress: true,
+    showPercentage: false,
+  });
 
-	//Preloader
+  //Parallax fade on scroll
 
-	Royal_Preloader.config({
-		mode           : 'progress',
-		background     : '#ffffff',
-		showProgress   : true,
-		showPercentage : false
-	});	
+  function scrollBanner() {
+    $(document).scroll(function () {
+      var scrollPos = $(this).scrollTop();
+      $(".parallax-fade-top").css({
+        top: scrollPos / 3 + "px",
+        opacity: 1 - scrollPos / 350,
+      });
+    });
+  }
+  scrollBanner();
 
-	
-	//Parallax fade on scroll	
-	
-	function scrollBanner() {
-	  $(document).scroll(function(){
-		var scrollPos = $(this).scrollTop();
-		$('.parallax-fade-top').css({
-		  'top' : (scrollPos/3)+'px',
-		  'opacity' : 1-(scrollPos/350)
-		});
-	  });    
-	}
-	scrollBanner();	
+  /* Scroll Animation */
 
-	
-	/* Scroll Animation */
-	
-	window.scrollReveal = new scrollReveal();
-	
-	
-	jQuery(document).ready(function(){	
-	
-		$('#projects-grid').children().hover(function() {
-		$(this).siblings().stop().fadeTo(400,0.2);
-		}, function() {
-			$(this).siblings().stop().fadeTo(400,1);
-		});
+  window.scrollReveal = new scrollReveal();
 
-		//Menu accordion
-		
-		var accordionsMenu = $('.cd-accordion-menu');
+  jQuery(document).ready(function () {
+    $("#projects-grid")
+      .children()
+      .hover(
+        function () {
+          $(this).siblings().stop().fadeTo(400, 1);
+        },
+        function () {
+          $(this).siblings().stop().fadeTo(400, 1);
+        }
+      );
 
-		if( accordionsMenu.length > 0 ) {
-			
-			accordionsMenu.each(function(){
-				var accordion = $(this);
-				//detect change in the input[type="checkbox"] value
-				accordion.on('change', 'input[type="checkbox"]', function(){
-					var checkbox = $(this);
-					console.log(checkbox.prop('checked'));
-					( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
-				});
-			});
-		}
-	
-		
-		//Scroll back to top
-	
-		var offset = 300;
-		var duration = 300;
-		jQuery(window).scroll(function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.scroll-to-top').fadeIn(duration);
-			} else {
-				jQuery('.scroll-to-top').fadeOut(duration);
-			}
-		});
-				
-		jQuery('.scroll-to-top').on('click', function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		})
-		
-				
-		//Tooltip
+    //Menu accordion
 
-		$(".tipped").tipper();
+    var accordionsMenu = $(".cd-accordion-menu");
 
+    if (accordionsMenu.length > 0) {
+      accordionsMenu.each(function () {
+        var accordion = $(this);
+        //detect change in the input[type="checkbox"] value
+        accordion.on("change", 'input[type="checkbox"]', function () {
+          var checkbox = $(this);
+          console.log(checkbox.prop("checked"));
+          checkbox.prop("checked")
+            ? checkbox
+                .siblings("ul")
+                .attr("style", "display:none;")
+                .slideDown(300)
+            : checkbox
+                .siblings("ul")
+                .attr("style", "display:block;")
+                .slideUp(300);
+        });
+      });
+    }
 
-		/* Portfolio Sorting */
+    //Scroll back to top
 
-		(function ($) { 
-		
-		
-			var container = $('#projects-grid');
-			
-			
-			function getNumbColumns() { 
-				var winWidth = $(window).width(), 
-					columnNumb = 1;
-				
-				
-				if (winWidth > 1500) {
-					columnNumb = 4;
-				} else if (winWidth > 1200) {
-					columnNumb = 3;
-				} else if (winWidth > 900) {
-					columnNumb = 2;
-				} else if (winWidth > 600) {
-					columnNumb = 2;
-				} else if (winWidth > 300) {
-					columnNumb = 1;
-				}
-				
-				return columnNumb;
-			}
-			
-			
-			function setColumnWidth() { 
-				var winWidth = $(window).width(), 
-					columnNumb = getNumbColumns(), 
-					postWidth = Math.floor(winWidth / columnNumb);
+    var offset = 300;
+    var duration = 300;
+    jQuery(window).scroll(function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".scroll-to-top").fadeIn(duration);
+      } else {
+        jQuery(".scroll-to-top").fadeOut(duration);
+      }
+    });
 
-			}
-			
-			$('#portfolio-filter #filter a').click(function () { 
-				var selector = $(this).attr('data-filter');
-				
-				$(this).parent().parent().find('a').removeClass('current');
-				$(this).addClass('current');
-				
-				container.isotope( { 
-					filter : selector 
-				});
-				
-				setTimeout(function () { 
-					reArrangeProjects();
-				}, 300);
-				
-				
-				return false;
-			});
-			
-			function reArrangeProjects() { 
-				setColumnWidth();
-				container.isotope('reLayout');
-			}
-			
-			
-			container.imagesLoaded(function () { 
-				setColumnWidth();
-				
-				
-				container.isotope( { 
-					itemSelector : '.portfolio-box-1', 
-					layoutMode : 'masonry', 
-					resizable : false 
-				} );
-			} );
-			
-			
-		
-			
-		
-			$(window).on('debouncedresize', function () { 
-				reArrangeProjects();
-				
-			} );
-			
-		
-		} )(jQuery);
-		
-	});	
- 
-	/* DebouncedResize Function */
-		(function ($) { 
-			var $event = $.event, 
-				$special, 
-				resizeTimeout;
-			
-			
-			$special = $event.special.debouncedresize = { 
-				setup : function () { 
-					$(this).on('resize', $special.handler);
-				}, 
-				teardown : function () { 
-					$(this).off('resize', $special.handler);
-				}, 
-				handler : function (event, execAsap) { 
-					var context = this, 
-						args = arguments, 
-						dispatch = function () { 
-							event.type = 'debouncedresize';
-							
-							$event.dispatch.apply(context, args);
-						};
-					
-					
-					if (resizeTimeout) {
-						clearTimeout(resizeTimeout);
-					}
-					
-					
-					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
-				}, 
-				threshold : 150 
-			};
-		} )(jQuery);
+    jQuery(".scroll-to-top").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
 
-	
- 
-  })(jQuery); 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+    //Tooltip
 
+    $(".tipped").tipper();
 
+    /* Portfolio Sorting */
 
+    (function ($) {
+      var container = $("#projects-grid");
 
+      function getNumbColumns() {
+        var winWidth = $(window).width(),
+          columnNumb = 1;
 
-	
+        if (winWidth > 1500) {
+          columnNumb = 4;
+        } else if (winWidth > 1200) {
+          columnNumb = 3;
+        } else if (winWidth > 900) {
+          columnNumb = 2;
+        } else if (winWidth > 600) {
+          columnNumb = 2;
+        } else if (winWidth > 300) {
+          columnNumb = 1;
+        }
+
+        return columnNumb;
+      }
+
+      function setColumnWidth() {
+        var winWidth = $(window).width(),
+          columnNumb = getNumbColumns(),
+          postWidth = Math.floor(winWidth / columnNumb);
+      }
+
+      $("#portfolio-filter #filter a").click(function () {
+        var selector = $(this).attr("data-filter");
+
+        $(this).parent().parent().find("a").removeClass("current");
+        $(this).addClass("current");
+
+        container.isotope({
+          filter: selector,
+        });
+
+        setTimeout(function () {
+          reArrangeProjects();
+        }, 300);
+
+        return false;
+      });
+
+      function reArrangeProjects() {
+        setColumnWidth();
+        container.isotope("reLayout");
+      }
+
+      container.imagesLoaded(function () {
+        setColumnWidth();
+
+        container.isotope({
+          itemSelector: ".portfolio-box-1",
+          layoutMode: "masonry",
+          resizable: false,
+        });
+      });
+
+      $(window).on("debouncedresize", function () {
+        reArrangeProjects();
+      });
+    })(jQuery);
+  });
+
+  /* DebouncedResize Function */
+  (function ($) {
+    var $event = $.event,
+      $special,
+      resizeTimeout;
+
+    $special = $event.special.debouncedresize = {
+      setup: function () {
+        $(this).on("resize", $special.handler);
+      },
+      teardown: function () {
+        $(this).off("resize", $special.handler);
+      },
+      handler: function (event, execAsap) {
+        var context = this,
+          args = arguments,
+          dispatch = function () {
+            event.type = "debouncedresize";
+
+            $event.dispatch.apply(context, args);
+          };
+
+        if (resizeTimeout) {
+          clearTimeout(resizeTimeout);
+        }
+
+        execAsap
+          ? dispatch()
+          : (resizeTimeout = setTimeout(dispatch, $special.threshold));
+      },
+      threshold: 150,
+    };
+  })(jQuery);
+})(jQuery);
--- router/viewRouter.js
+++ router/viewRouter.js
@@ -1,14 +1,14 @@
 module.exports = (express) => {
   const router = express.Router();
 
-  // define middleware
-  //   function isLoggedIn(req, res, next) {
-  // check if user is currently logged in
-  // if (req.isAuthenticated()) {
-  //   return next();
-  // }
-  // if not logged in, redirect to the landing page
-  // res.render("login");
+  // // define middleware
+  // function isLoggedIn(req, res, next) {
+  //   // check if user is currently logged in
+  //   if (req.isAuthenticated()) {
+  //     return next();
+  //   }
+  //   // if not logged in, redirect to the landing page
+  //   res.render("login");
   // }
 
   router.get("/", (req, res) => {
@@ -21,6 +21,14 @@ module.exports = (express) => {
 
   router.get("/contact", (req, res) => {
     res.render("contact");
+  });
+
+  router.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  router.get("/edit", (req, res) => {
+    res.render("edit");
   });
 
   return router;
