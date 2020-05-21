acd4b84bd2c78c5581c3dbb8957f76ede7981960
FoongL <leung.foong@gmail.com>
2020-05-21 14:55:58 +0800

    updated seed files

--- seeds/initial_data.js
+++ seeds/initial_data.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {email: 'andrew@drewtodraw.com', password: 'drewtodraw'}
+        {email: 'andrew@drewtodraw.com', password: '$2b$10$S8Sf4AHIT1MuevE22ZQIDOg2e2BNc0906SYyRGySHlp0xFZms0XlG'}
       ]);
     });
 };
